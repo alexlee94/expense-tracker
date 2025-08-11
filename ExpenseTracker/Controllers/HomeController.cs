e83b95ad98e27ea52015a3452b6cabc999ae2f6c
Alex Lee <alexlee94@gmail.com>
2025-08-11 13:56:57 -0400

    Add CreateEditExpense view for creating and editing expenses

--- ExpenseTracker/Controllers/HomeController.cs
+++ ExpenseTracker/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace ExpenseTracker.Controllers
             return View();
         }
 
+        public IActionResult CreateEditExpense()
+        {
+            return View();
+        }
+
+        public IActionResult CreateEditExpenseForm(Expense model)
+        {
+            return RedirectToAction("Expenses");
+        }
+
         public IActionResult Privacy()
         {
             return View();
